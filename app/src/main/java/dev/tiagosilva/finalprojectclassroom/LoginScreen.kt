ae37ea731eb810124e8df6e0979030d0c13cf96c
Tiago Silva <tiagoluizrs@gmail.com>
2023-03-06 21:35:02 -0300

    Login google

--- app/src/main/java/dev/tiagosilva/finalprojectclassroom/LoginScreen.kt
+++ app/src/main/java/dev/tiagosilva/finalprojectclassroom/LoginScreen.kt
@@ -6,11 +6,24 @@ import android.os.Bundle
 import android.view.View
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 
 class LoginScreen : AppCompatActivity() {
 
     lateinit var etEmail: EditText
     lateinit var etPassword: EditText
+
+    val Req_Code:Int=123;
+    lateinit var mGoogleSignInClient: GoogleSignInClient;
+    private lateinit var firebaseAuth: FirebaseAuth;
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,6 +33,16 @@ class LoginScreen : AppCompatActivity() {
         etEmail = findViewById<EditText>(R.id.etEmail)
         etPassword = findViewById<EditText>(R.id.etPassword)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build();
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
+
         findViewById<View>(R.id.registerView).setOnClickListener{
             val activity = Intent(this, CreateAccount::class.java);
             startActivity(activity);
@@ -27,10 +50,49 @@ class LoginScreen : AppCompatActivity() {
 
         findViewById<View>(R.id.Signin).setOnClickListener {
             Toast.makeText(this, R.string.login_com_google, Toast.LENGTH_SHORT).show()
+            signInGoogle();
         }
 
         findViewById<View>(R.id.btnEnter).setOnClickListener {
             Toast.makeText(this, "Entrando com login normal", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun signInGoogle(){
+        val signIntent: Intent = mGoogleSignInClient.signInIntent;
+        startActivityForResult(signIntent, Req_Code);
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == Req_Code){
+            val result = GoogleSignIn.getSignedInAccountFromIntent(data);
+            handleResult(result);
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>){
+        try{
+            val account: GoogleSignInAccount? = completedTask.getResult(ApiException::class.java);
+            Toast.makeText(this, "Logado com sucesso", Toast.LENGTH_SHORT).show();
+            if(account != null){
+                UpdateUser(account)
+            }
+        }catch (e: ApiException){
+            println(e)
+            Toast.makeText(this, "Falha ao logar", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private fun UpdateUser(account: GoogleSignInAccount){
+        val credential = GoogleAuthProvider.getCredential(account.idToken, null);
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {task ->
+            if(task.isSuccessful){
+                val intent = Intent(this, MainActivity::class.java);
+                startActivity(intent);
+                finish()
+            }
         }
     }
 }
--- app/src/main/java/dev/tiagosilva/finalprojectclassroom/MainActivity.kt
+++ app/src/main/java/dev/tiagosilva/finalprojectclassroom/MainActivity.kt
@@ -3,32 +3,86 @@ package dev.tiagosilva.finalprojectclassroom
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.SparseBooleanArray
 import android.view.View
 import android.widget.ArrayAdapter
+import android.content.Context
+import android.content.SharedPreferences
+import com.google.gson.Gson
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var sharedPreferences: SharedPreferences
+    private var gson = Gson()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        var listViewTasks = findViewById<android.widget.ListView>(R.id.listViewTasks);
-        var createTask = findViewById<android.widget.EditText>(R.id.createTask);
+        sharedPreferences = getSharedPreferences("lista_de_tarefas", Context.MODE_PRIVATE)
 
-        var itemList = arrayListOf<String>();
-        var adapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, itemList);
+        val listViewTasks = findViewById<android.widget.ListView>(R.id.listViewTasks);
+        val createTask = findViewById<android.widget.EditText>(R.id.createTask);
+
+//        val itemList = arrayListOf<String>();
+        val itemList = getData();
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, itemList);
+
+        listViewTasks.adapter = adapter;
+        adapter.notifyDataSetChanged()
 
         findViewById<View>(R.id.add).setOnClickListener {
             itemList.add(createTask.text.toString());
             listViewTasks.adapter = adapter;
             adapter.notifyDataSetChanged()
 
+            saveData(itemList)
             createTask.text.clear()
+        }
+
+        findViewById<View>(R.id.delete).setOnClickListener {
+            val position: SparseBooleanArray = listViewTasks.checkedItemPositions;
+            val count = listViewTasks.count;
+            var item = count - 1;
+            while(item >= 0){
+                if(position.get(item)){
+                    adapter.remove(itemList.get(item))
+                }
+                item--;
+            }
+
+            saveData(itemList)
+            position.clear();
+            adapter.notifyDataSetChanged()
+        }
+
+        findViewById<View>(R.id.clear).setOnClickListener{
+            itemList.clear()
+            saveData(itemList)
+            adapter.notifyDataSetChanged()
         }
 
         findViewById<View>(R.id.logout).setOnClickListener{
             val activity = Intent(this, LoginScreen::class.java);
             startActivity(activity);
         }
+    }
+
+    private fun getData(): ArrayList<String> {
+        val arrayJson = sharedPreferences.getString("lista", null);
+        return if(arrayJson.isNullOrEmpty()){
+            arrayListOf();
+        }else{
+            gson.fromJson(arrayJson, object: TypeToken<ArrayList<String>>(){}.type)
+        }
+    }
+
+    private fun saveData(array: ArrayList<String>){
+        val arrayJson = gson.toJson(array);
+        val editor = sharedPreferences.edit();
+        editor.putString("lista", arrayJson);
+        editor.apply();
     }
 }
